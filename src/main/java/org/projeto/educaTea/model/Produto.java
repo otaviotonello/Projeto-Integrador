52152ccb22b8d37baaac2bacbcb6cc958c7d4666
Bruna <bruna41bergami@gmail.com>
2021-12-06 11:47:54 -0300

    @bruna41 [Task: Adicionando Controller do produto]

--- src/main/java/org/projeto/educaTea/model/Produto.java
+++ src/main/java/org/projeto/educaTea/model/Produto.java
@@ -3,18 +3,22 @@ package org.projeto.educaTea.model;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@Entity
+@Table (name = "tb_produto")
 public class Produto {
-	
+
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -67,10 +71,13 @@ public class Produto {
 	public void setNome_instrutor(String nome_instrutor) {
 		this.nome_instrutor = nome_instrutor;
 	}
-	
-	
-	
-	
 
+	public List<Categoria> getCategoria() {
+		return categoria;
+	}
 
+	public void setCategoria(List<Categoria> categoria) {
+		this.categoria = categoria;
+	}
+	
 }

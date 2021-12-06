cb17f934568e8eda9ccf73393e7628e0ce71389a
unknown <borellipedroluiz@gmail.com>
2021-12-06 10:24:33 -0300

    @pedro-borelli [Task: Adicionando controller e repository Categoria]

--- src/main/java/org/projeto/educaTea/repository/CategoriaRepository.java
+++ src/main/java/org/projeto/educaTea/repository/CategoriaRepository.java
@@ -1,5 +1,12 @@
 package org.projeto.educaTea.repository;
 
-public interface CategoriaRepository {
+import java.util.List;
 
+import org.projeto.educaTea.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
+	public List<Categoria> findAllByNomeContainingIgnoreCase(String nome);
 }

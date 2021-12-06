33de5889c4517bbf706b1b7a7072341afe28c808
Bianca <biancasouzarocha@outlook.com>
2021-12-06 11:00:40 -0300

    @BiancaSouzaRocha [Task: Adicionando produto Model e Repository]

--- src/main/java/org/projeto/educaTea/repository/ProdutoRepository.java
+++ src/main/java/org/projeto/educaTea/repository/ProdutoRepository.java
@@ -1,5 +1,15 @@
 package org.projeto.educaTea.repository;
 
-public interface ProdutoRepository {
+import java.util.List;
+
+
+import org.projeto.educaTea.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNome_ProdutoContainingIgnoreCase(String nome_produto);
+	
 
 }

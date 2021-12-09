cda569387903c9dade06bb0175036fbae5ba49cf
Bruna <bruna41bergami@gmail.com>
2021-12-09 12:01:24 -0300

    @bruna41 [Task: Criando UsuarioRepository e documentação]

--- src/main/java/org/projeto/educaTea/repository/UsuarioRepository.java
+++ src/main/java/org/projeto/educaTea/repository/UsuarioRepository.java
@@ -1,5 +1,21 @@
 package org.projeto.educaTea.repository;
 
-public interface UsuarioRepository {
+import java.util.List;
+
+import org.projeto.educaTea.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @param String
+ * @return List<Usuario>
+ * @author Bruna Bergami
+ * @version 1.0
+ * @see CategoriaRepository
+ * @see ProdutoRepository 
+ * @see Usuario
+ */
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public List<Usuario> findAllByNomeUsuarioContainingIgnoreCase(String nomeUsuario);
 
 }

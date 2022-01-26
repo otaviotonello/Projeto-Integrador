0e0f27dbecbe40398f411f3f71b8e6f9b28039e4
Bruna <bruna41bergami@gmail.com>
2022-01-26 16:08:08 -0300

    @bruna41 [Atualizando ambiente heroku]

--- src/main/java/org/projeto/educaTea/model/Produto.java
+++ src/main/java/org/projeto/educaTea/model/Produto.java
@@ -62,6 +62,11 @@ public class Produto {
 	@JsonIgnoreProperties ("produto")
 	private Categoria categoria;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+
+	
 	public long getId() {
 		return id;
 	}
@@ -101,4 +106,14 @@ public class Produto {
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
 }
